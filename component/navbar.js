3d4e7e5941554ca9677f8670f2465a99147c1cbc
Nilesh Agrawal <nilesh_fw12_098@masai.school>
2021-11-08 16:48:29 +0530

    Completed

--- component/navbar.js
+++ component/navbar.js
@@ -12,4 +12,62 @@ function navbar() {
 
 }
 
-export default navbar;
+function logUser() {
+
+    let loggedUser = localStorage.getItem("loggedUser");
+
+    if (loggedUser !== null && loggedUser !== "") {
+
+        let user_option = document.querySelector("#navbar > div > ul:nth-of-type(2)");
+
+        user_option.innerHTML = "";
+
+        let name = document.createElement("li");
+
+        name.textContent = loggedUser;
+
+        let logout = document.createElement("li");
+
+        logout.textContent = "Logout";
+        logout.onclick = () => {
+            userLogout();
+        }
+
+        let li3 = document.createElement("li");
+        let a3 = document.createElement("a");
+        a3.href = "cart.html";
+        a3.textContent = "Cart";
+        li3.append(a3);
+
+        user_option.append(li3, name, logout);
+
+    }
+}
+
+function userLogout() {
+    localStorage.setItem("loggedUser", "");
+    let li1 = document.createElement("li");
+    let a1 = document.createElement("a");
+    a1.href = "login.html";
+    a1.textContent = "Login";
+    li1.append(a1);
+
+    let li2 = document.createElement("li");
+    let a2 = document.createElement("a");
+    a2.href = "signup.html";
+    a2.textContent = "Signup";
+    li2.append(a2);
+
+    let li3 = document.createElement("li");
+    let a3 = document.createElement("a");
+    a3.href = "cart.html";
+    a3.textContent = "Cart";
+    li3.append(a3);
+
+    let user_option = document.querySelector("#navbar > div > ul:nth-of-type(2)");
+
+    user_option.innerHTML = "";
+    user_option.append(li3, li1, li2);
+}
+
+export { navbar, logUser, userLogout };
