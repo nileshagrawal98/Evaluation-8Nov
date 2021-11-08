85f38102a918685cb7a07a130c0506220114530f
Nilesh Agrawal <nilesh_fw12_098@masai.school>
2021-11-08 16:19:56 +0530

    cart

---
+++ component/navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+    return `<div class="container">
+    <ul>
+        <li><a href="menu.html">Menu</a></li>
+    </ul>
+    <ul>
+        <li><a href="cart.html">Cart</a></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="signup.html">SignUp</a></li>
+    </ul>
+</div>`
+
+}
+
+export default navbar;
